ade1269dc419aa4d4d69b8da84b9865ec7cfcd44
AnneCride <acpetit@icloud.com>
2017-11-10 12:04:19 +0100

    connexion heroku

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
- 	host     : ' sql11.freemysqlhosting.net',
+ 	host     : 'sql11.freemysqlhosting.net',
 	user     : 'sql11204153',
 	password : 'pJ9hQAe837',
 	database : 'sql11204153'
